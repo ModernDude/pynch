010165c456bd8fffbf597381189dca95cc03c966
Jeff Sigmon <jeff@jeffsigmon.com>
2011-05-17 09:00:37 -0500

    Refactored follow calls

--- src/pynch/core.clj
+++ src/pynch/core.clj
@@ -1,10 +1,13 @@
 (ns pynch.core
-  (:require [net.cgrand.enlive-html :as html])
-  (:require [clj-time.core :as tm]))
+  (:use [net.cgrand.enlive-html :only
+         [text has re-pred select attr-starts text-node
+          text-pred html-resource texts let-select nth-child html-snippet]])
+  (:require [clj-time.core :as tm])
+  (:require [clojure.string :as str]))
 
 
 (def *hn-url* "http://news.ycombinator.com/")
-(def *crawl-delay* 30000)
+(def *crawl-delay* 0);30000)
 
 (defn to-int [s]
  (Integer/parseInt s))
@@ -49,75 +52,125 @@ symbol resolve))]
      (now))))
 
 
-(defn- get-next-page-uri [ns uri]
+(defmulti get-res-uri class)
+(defmethod get-res-uri :default [res] (java.net.URI. *hn-url*))
+(defmethod get-res-uri java.net.URI [res] res)
+(defmethod get-res-uri java.net.URL [res]
+  (println "Convert to uri")
+  (flush)
+  (.toURI res))
+  
+
+
+(defn- get-more-link [ns]
+  (-> (select ns [:td.title [:a (attr-starts :href "/x?fnid")]])
+      first :attrs :href))
+
+(defn- get-next-page-uri [res more-link]
   "Find the next absolute uri based on the given uri and the
    more link found in ns."
- (Thread/sleep *crawl-delay*)
- (let [node (html/select ns [:td.title
-                             [:a (html/attr-starts :href "/x?fnid")]])
-       link (-> node first :attrs :href)]
-   (.resolve uri link)))
+  (Thread/sleep *crawl-delay*)
+  (let [new-res (get-res-uri res)]
+    (if (= "file" (.getScheme new-res))
+      (.resolve new-res (str/replace more-link #"^/" ""))
+      (.resolve new-res more-link))))
 
-(defn- get-submissions-map [ns]
+
+
+
+
+(def extractors
+  {:sub-ordinals
+   {:selector [[:.title (has [(re-pred #"^[0-9]+\.{1}$")])]]
+    :transforms {:ordinal #(-> % text (re-first-seq-digits 0))}}
+   :sub-titles
+   {:selector [:.title :a]
+    :transforms {:title #(text %)
+                :sub-url #(-> % :attrs :href)}}
+   :sub-points
+   {:selector [:td.subtext [:span (attr-starts :id "score_")]]
+    :transforms {:points #(-> % text (re-first-seq-digits 0))}}
+   :sub-users
+   {:selector [:td.subtext [:a (attr-starts :href "user?")]]
+    :transforms {:user #(text %)}}
+   :sub-com-urls
+   {:selector [:td.subtext [:a (attr-starts :href "item?")]]
+    :transforms {:com-url #(-> % :attrs :href)
+                 :com-count #(-> % text (re-first-seq-digits 0))}}
+   :sub-times
+   {:selector [:td.subtext [text-node (text-pred #(re-find #"ago\s*\|" %))]]
+    :transforms {:time #(hn-time-to-dt %)}}})
+
+
+(defn- extract-seq [r extractor transform]
+  ""
+  (let [ext (-> extractors extractor)]
+    (map (-> ext :transforms transform)
+       (select r (-> ext :selector)))))
+
+
+(defn make-sub [ord title sub-url sub-time points
+                user com-url com-cnt]
+  {:ordinal ord
+   :title title
+   :submission-url sub-url
+   :submission-time sub-time
+   :points points
+   :user user
+   :comments-url com-url
+   :comments-count com-cnt})
+
+
+(defn- get-subs-map [ns]
   "Get a sequext of maps for each submission found in ns."
- (html/let-select
-  ns
-  [ordinals [[:.title (html/has [(html/re-pred #"^[0-9]+\.{1}$")])]]
-   titles [:.title :a]
-   points [:td.subtext [ :span (html/attr-starts :id "score_")]]
-   users [:td.subtext [ :a (html/attr-starts :href "user?")]]
-   com-links [:td.subtext [ :a (html/attr-starts :href "item?")]]
-   sub-times [:td.subtext
-              [html/text-node (html/text-pred #(re-find #"ago\s*\|" %)) ]]]
-  (map (fn [ordinal title point user com-link sub-time]
-   {:ordinal (-> ordinal html/text (re-first-seq-digits 0))
-    :title (html/text title)
-    :submission-url (-> title :attrs :href)
-    :submission-time (hn-time-to-dt sub-time)
-    :points (-> point html/text (re-first-seq-digits 0))
-    :user (html/text user)
-    :comments-url (-> com-link :attrs :href)
-    :comments-count (-> com-link html/text (re-first-seq-digits 0) )})
-       ordinals titles points users com-links sub-times)))
+  (map #(make-sub %1 %2 %3 %4 %5 %6 %7 %8)
+       (extract-seq ns :sub-ordinals :ordinal)
+       (extract-seq ns :sub-titles :title)
+       (extract-seq ns :sub-titles :sub-url)
+       (extract-seq ns :sub-times :time)
+       (extract-seq ns :sub-points :points)
+       (extract-seq ns :sub-users :user)
+       (extract-seq ns :sub-com-urls :com-url)
+       (extract-seq ns :sub-com-urls :com-count)))
 
 
-(defn get-subs [x]
+(defn get-subs [res]
   "Returns a map of all submissions located at or within
-   resource x. The type of x can be any of the following
+   resource r. The type of x can be any of the following
    String, java.io.FileInputStream, java.io.Reader,
    java.io.InputStream, java.net.URL, java.net.URI"
- (-> x html/html-resource get-submissions-map first))
+ (-> res html-resource get-subs-map))
 
-(defn get-submissions-follow [uri]
+(defn get-subs-follow [res]
   "Loads the hacker news submission list given by the resource
    uri and returns a lazy list representing the parsed data."
  (lazy-seq
-  (let [ns (html/html-resource uri)
-        next-uri (get-next-page-uri ns uri)]
+  (let [ns (html-resource res)
+        next-uri (get-next-page-uri res (get-more-link ns))]
+    (println next-uri)
     (concat
-     (flatten
-      (get-submissions-map ns))
-     (get-submissions-follow next-uri)))))
+     (flatten (get-subs-map ns))
+     (get-subs-follow next-uri)))))
 
 
 (defn- get-comment-paragraphs [ns]
   "Returns a sequece of strings representing each paragraph in the comment."
- (->> (html/select ns #{[:p] [:font] }) html/texts (drop-last 2)))
+ (->> (select ns #{[:p] [:font] }) texts (drop-last 2)))
 
 (defn- get-submission-comments-dtl-map [ns]
   "Return a map of all submission comment details found in ns"
  (flatten
-  (html/let-select
+  (let-select
    ns
-   [users [:.default :.comhead [:a (html/attr-starts :href "user?")]]
+   [users [:.default :.comhead [:a (attr-starts :href "user?")]]
     com-times [:.default :.comhead
-               [html/text-node (html/text-pred #(re-find #"ago\s*|" %))]]
-    com-links [:.default :.comhead [:a (html/attr-starts :href "item?")]]
+               [text-node (text-pred #(re-find #"ago\s*|" %))]]
+    com-links [:.default :.comhead [:a (attr-starts :href "item?")]]
     comments [:.default]; (html/has [:*])]]
     ]
 
     (map (fn [user com-time com-link comment]
-          {:user (html/text user)
+          {:user (text user)
            :comment-time (hn-time-to-dt com-time)
            :comment-link (-> com-link :attrs :href)
            :comment (get-comment-paragraphs comment)
@@ -128,22 +181,22 @@ symbol resolve))]
 
 (defn- get-submission-comments-hdr-map [ns]
   "Return a map containing all submission comments found in ns."
- (html/let-select
+ (let-select
   ns
   [titles [:td.title]
-   points [:td.subtext [:span (html/attr-starts :id "score_")]]
-   users [:td.subtext [:a (html/attr-starts :href "user?")]]
-   com-links [:td.subtext [:a (html/attr-starts :href "item?")]]
+   points [:td.subtext [:span (attr-starts :id "score_")]]
+   users [:td.subtext [:a (attr-starts :href "user?")]]
+   com-links [:td.subtext [:a (attr-starts :href "item?")]]
    sub-times [:td.subtext
-              [html/text-node (html/text-pred #(re-find #"ago\s*\|" %))]]
-   notes [:table :tr :td :table [:tr (html/nth-child 4)]]]
+              [text-node (text-pred #(re-find #"ago\s*\|" %))]]
+   notes [:table :tr :td :table [:tr (nth-child 4)]]]
   (map (fn [title point user com-link sub-time note]
-   {:title (html/text title)
+   {:title (text title)
     :submission-time (hn-time-to-dt sub-time)
-    :points (-> points html/text (re-first-seq-digits 0))
-    :user (html/text user)
-    :comments-count (-> com-links html/text (re-first-seq-digits 0))
-    :notes (html/text note)
+    :points (-> points text (re-first-seq-digits 0))
+    :user (text user)
+    :comments-count (-> com-links text (re-first-seq-digits 0))
+    :notes (text note)
     :comments (get-submission-comments-dtl-map ns)})
        titles points users com-links sub-times notes)))
 
@@ -154,10 +207,10 @@ symbol resolve))]
   "The string containing html provided by s will be parsed
    and a map will be returned containing all details relating 
    to discussions on hacker news for the specified submission."
- (-> s html/html-snippet get-submission-comments-hdr-map))
+ (-> s html-snippet get-submission-comments-hdr-map))
 
 (defmethod get-comments java.net.URI [uri]
   "The resource provided by uri will be loaded and parsed 
    and a map will be returned containing all details relating
    to discussions on hacker news for the specified submission."
- (-> uri html/html-resource get-submission-comments-hdr-map first))
+ (-> uri html-resource get-submission-comments-hdr-map first))
